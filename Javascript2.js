898191508dd0a051639b76826b85cd1765f3627a
RebeccaMitchelle <rebeccaamitchelle@gmail.com>
2022-10-20 13:14:27 +0300

    Javascript2

---
+++ Javascript2.js
@@ -0,0 +1,15 @@
+//An array 
+var names=["Brenda","Trish","Belinda","Bianka","Grace","Mitchelle","Blessing","Favour","Treasure","Nathaniel"];
+
+//For I
+
+
+for( let i= 0; i>=0; ++i) {   
+    if(i ==5){
+        console.log("This is my name")
+        break;
+    }
+        console.log(names[i])
+}
+
+
